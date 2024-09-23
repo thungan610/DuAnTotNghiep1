8d3ee65c22aa69d6f117d6691a2d48fc2e8f53f7
ngocphat922004 <130296645+ngocphat922004@users.noreply.github.com>
2024-09-23 22:19:24 +0700

    home

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { View, Text, TextInput } from 'react-native'
 import Welcome from './src/Page/Wellcome'
 import Login from './src/Page/Login'
+import HomeScreen from './src/Page/HomeScreen'
 
 const App = () => {
 
   return (
     <View>
-      <Login/>
+      <HomeScreen/>
     </View>
   )
 }
