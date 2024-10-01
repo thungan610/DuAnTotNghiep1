d38ec51ebf2987cc5c361ee988804d394e2aad89
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-10-01 19:54:16 +0700

    DinhNhan push code

--- src/Page/Profile/index.js
+++ src/Page/Profile/index.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const Profile = () => {
     return (
         <View>
-            <Text>Profile</Text>
+            <Text>helllo nnhaa đây</Text>
         </View>
     );
 };
