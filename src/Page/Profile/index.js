d1748b9017ded43af6ebaa0d10affcb6351cff5b
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-10-01 21:38:40 +0700

    Update index.js

--- src/Page/Profile/index.js
+++ src/Page/Profile/index.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const Profile = () => {
     return (
         <View>
-            <Text>alo alo có chưa</Text>
+            <Text>alo alo có a</Text>
         </View>
     );
 };
