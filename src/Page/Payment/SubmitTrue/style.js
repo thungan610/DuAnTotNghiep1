f6c57f6cfb74fd4769d67a9a65396ccc0d00f9ab
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-10-05 01:26:00 +0700

    Dinh Nhan Push Code Pay

---
+++ src/Page/Payment/SubmitTrue/style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const SubmitTrueStyle = StyleSheet.create({
+    container:{
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    },
+    img:{
+        width: 296,
+        height:328
+    },
+    text:{
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "black",
+        fontFamily: "Poppins",
+        textAlign:'center',
+    }
+})
+
+export default SubmitTrueStyle
