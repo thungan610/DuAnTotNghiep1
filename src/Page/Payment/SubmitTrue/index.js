87e735eb8f3f933d3d04438ed1058cfb707797ba
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-10-08 23:28:41 +0700

    DInhNhan Push code 2 màn hình Pay

--- src/Page/Payment/SubmitTrue/index.js
+++ src/Page/Payment/SubmitTrue/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import SubmitTrueStyle from "./style";
 import AddAdressStyle from "../AddAdress/style";
-const SubmitTrue = () => {
+const SubmitTrue = (prop) => {
+    const { navigation } = prop
+    const BackRight = () => {
+        navigation.goBack()
+    }
+    const HandPayment = () => {
+        navigation.navigate('Payment')
+    }
     return (
         <View style={SubmitTrueStyle.container}>
             <Image style={SubmitTrueStyle.img} source={require("../../../assets/notifi/imagelike.png")} />
             <Text style={SubmitTrueStyle.text}>Thêm địa chỉ thành công</Text>
-            <TouchableOpacity style={AddAdressStyle.button}>
+            <TouchableOpacity onPress={HandPayment} style={AddAdressStyle.button}>
                 <Text style={AddAdressStyle.buttonText}>TIẾP TỤC THANH TOÁN</Text>
             </TouchableOpacity>
         </View>
