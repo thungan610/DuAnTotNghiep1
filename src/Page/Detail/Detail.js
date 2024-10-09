5a7cd86a78bf0838048b6a373ee65c8fdd434499
thungan610 <156633890+thungan610@users.noreply.github.com>
2024-10-10 01:12:23 +0700

    detail,detailDiscout,detaillbotle

--- src/Page/Detail/Detail.js
+++ src/Page/Detail/Detail.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Image,TouchableOpacity,Button } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity, Button, ScrollView } from 'react-native';
 import PagerView from 'react-native-pager-view';
-
+import styles from './style';
 export default Detail = () => {
 
     const [selectedIndex, setSelectedIndex] = useState(0);
@@ -31,7 +31,7 @@ export default Detail = () => {
                     style={{
                         width: 10, height: 10,
                         borderRadius: 5,
-                        backgroundColor: selectedIndex === index ? 'blue' : 'gray',
+                        backgroundColor: selectedIndex === index ? 'black' : 'gray',
                         margin: 5, top: -50
                     }} />
             )
@@ -40,7 +40,11 @@ export default Detail = () => {
 
     return (
         <View style={styles.container}>
-
+            <View style={styles.head}>
+                <Image source={require('../../assets/notifi/backright.png')} />
+                <Image style={styles.iconcart} source={require('../../assets/home/cart.png')} />
+                <Image style={styles.iconnotifi} source={require('../../assets/home/notifi.png')} />
+            </View>
             <PagerView style={styles.pagerView}
                 initialPage={selectedIndex}>
                 {renderImages()}
@@ -57,86 +61,57 @@ export default Detail = () => {
                     <Text style={styles.textBody}>Bắp cải trắng</Text>
                     <Text style={styles.textkg}>1kg</Text>
                 </View>
-                {/* <View style={styles.butonView}>
-                    <TouchableOpacity style={styles.tout}>
-                    <Text style={styles.textTout}>1</Text>
+                <View style={styles.butonView}>
+                    <TouchableOpacity style={styles.minus}>
+                        <Text style={styles.textTout}>-</Text>
                     </TouchableOpacity>
-                </View> */}
+                    <TouchableOpacity style={styles.tout}>
+                        <Text style={styles.toutText}>1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.plus}>
+                        <Text style={styles.textTout}>+</Text>
+                    </TouchableOpacity>
+
+                    <Image style={styles.dolar} source={require('../../assets/Dollar.png')} />
+                    <Text style={styles.price}>19.000d</Text>
+                </View>
+                <View style={styles.underline}>
+                    <Text style={styles.textunderline}>Thông tin sản phẩm</Text>
+                </View>
+                <ScrollView style={styles.scroollview}>
+                    <View style={styles.viewScroll}>
+                        <Text style={styles.scrollText}>
+                            Sườn non là một loại thực phẩm phổ biến trên toàn thế giới, được sử dụng trong nhiều món ăn khác nhau.
+                        </Text>
+                        
+                    </View>
+                    <View style={styles.origin}> 
+                        <View style={styles.textoriginRow}>
+                        <Text style={styles.textorigin}>Xuất xứ  :</Text>
+                        <Text style={styles.textorigin}>Việt Nam</Text>
+                        </View>
+                        <View style={styles.textoriginRow}>
+                        <Text style={styles.textorigin}>Chất sơ  :</Text>
+                        <Text style={styles.textorigin}> Khoảng 2.2 gram trên 100 gram</Text>
+                        </View>
+                        <View style={styles.textoriginRow}>
+                        <Text style={styles.textorigin}>Bảo quản  :</Text>
+                        <Text style={styles.textorigin}> Bảo quản lạnh</Text>
+                        </View>
+                        <View style={styles.textoriginRow}>
+                        <Text style={styles.textorigin}>Công dụng :</Text>
+                        <Text style={styles.textorigin}>Chế biến món ăn</Text>
+                        </View>
+                    </View>
+                    
+                </ScrollView>
+                {/* buton */}
+                <TouchableOpacity style={styles.headerFooter}>
+                    <Text style={styles.textFooter}>Thêm vào giỏ hàng</Text>
+                </TouchableOpacity>
             </View>
             
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    next: {
-        right: 10,
-    },
-    previous: {
-        left: 10,
-    },
-    icon: {
-        position: 'absolute',
-        top: 150,
-    },
-    container: {
-        width: '100%',
-        height: '100%',
-        // backgroundColor: '#27AAE1'
-    },
-    pagerView: {
-        width: '100%',
-        height: 300,
-    },
-    body: {
-        width: '100%',
-        height: '50%',
-        // backgroundColor: '#37C5DF',
-        position: "relative",
-        borderTopLeftRadius: 50,
-        borderTopRightRadius: 50,
-    },
-    bodyText: {
-        width: '100%',
-        height: 50,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-
-
-    },
-    textBody: {
-        fontSize: 24,
-        color: 'back',
-        fontWeight: 'bold',
-        left: 30,
-        top: 10
-    },
-    textkg: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#8F8F8F',
-        top: 10,
-        left: -30
-    },
-    butonView:{
-        width:'100%',
-        height:50,
-        flexDirection:'row',
-        backgroundColor:'blue',
-    },
-    tout:{
-        width:'100%',
-        height:50,
-        backgroundColor:'while',
-        borderRadius:10,
-        flexDirection:'row'
-    },
-textTout:{
-    fontSize:18,
-    fontWeight:'bold',
-    color:'white',
-    top:10,
-    left:20
-}
-});
