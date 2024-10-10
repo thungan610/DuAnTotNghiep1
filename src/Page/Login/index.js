430b4ac2ef3433319bbb5b788fb8922195d6b344
ngocphat922004 <130296645+ngocphat922004@users.noreply.github.com>
2024-10-10 20:47:45 +0700

    update bắt lỗi

--- src/Page/Login/index.js
+++ src/Page/Login/index.js
@@ -3,62 +3,92 @@ import React, { useMemo, useState } from 'react';
 import { RadioGroup } from 'react-native-radio-buttons-group';
 import LoginStyle from './style';
 
-const Login = () => {
+const Login = (prop) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
     const [emailError, setEmailError] = useState('');
     const [passwordError, setPasswordError] = useState('');
     const [loginError, setLoginError] = useState('');
+    const [selectedId, setSelectedId] = useState();
+
 
     const BtnLogin = () => {
-        prop.navigation.navigate('AddProduct')
         let hasError = false;
 
+        // Reset lỗi
         setEmailError('');
         setPasswordError('');
         setLoginError('');
 
+        // Kiểm tra email
         if (email === '') {
             setEmailError("Vui lòng nhập Email");
-            setEmail('');
             hasError = true;
         } else if (!validateEmail(email)) {
             setEmailError("Email không hợp lệ!");
-            setEmail('');
             hasError = true;
         }
+
+        // Kiểm tra mật khẩu
         if (password === '') {
             setPasswordError("Vui lòng nhập mật khẩu");
-            setPassword('');
             hasError = true;
-        } else if (password.length < 6) {
-            setPasswordError("Mật khẩu phải có ít nhất 6 ký tự");
+        } else {
+            const passwordValidationError = validatePassword(password);
+            if (passwordValidationError) {
+                setPasswordError(passwordValidationError);
+                hasError = true;
+            }
+        }
+
+        if (hasError) {
+            setEmail('');
             setPassword('');
-            hasError = true;
+            return;
         }
 
         // Kiểm tra đăng nhập
-        if (!hasError) {
-            if (email !== "example@mail.com" || password !== "password") {
-                setLoginError("Email hoặc mật khẩu không đúng!");
-                setEmail('');
-                setPassword('');
-                return;
-            }
-
-            Alert.alert("Đăng nhập thành công");
+        if (email !== "cphat774@gmail.com" || password !== "Ngocphat123@") {
+            setLoginError("Email hoặc mật khẩu không đúng!");
+            setEmail('');
+            setPassword('');
+            return;
         }
-    }; 
 
-    // Kiểm tra email hợp lệ
-    const validateEmail = (email) => {
-        const re = /\S+@\S+\.\S+/;
-        return re.test(email);
+        if (!hasError) {
+            Alert.alert("Thông báo", "Đăng nhập thành công!");
+            setTimeout(() => {
+                prop.navigation.navigate('AddProduct');
+            }, 1000);
+        }
     };
 
-    const ClickRegister = () => {
-        prop.navigation.navigate('Register');
+    const validateEmail = (email) => {
+        // Biểu thức chính quy để kiểm tra định dạng email
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+    // Cập nhật hàm kiểm tra mật khẩu
+    const validatePassword = (password) => {
+        if (password.length < 8) {
+            return 'Phải có ít nhất 8 ký tự.';
+        }
+        if (!/\d/.test(password)) {
+            return 'Phải chứa ít nhất một số.';
+        }
+        if (!/[!@#$%^&*]/.test(password)) {
+            return 'Phải chứa ít nhất một ký tự đặc biệt.';
+        }
+        if (!/[A-Z]/.test(password)) {
+            return 'Phải chứa ít nhất một chữ cái in hoa.';
+        }
+        return '';
+    };
+    const handlePasswordChange = (text) => {
+        setPassword(text);
+        setPasswordError('');
+        setLoginError('');
     };
 
     const ClickForgotPass = () => {
@@ -66,108 +96,95 @@ const Login = () => {
     };
 
     const radioButtons = useMemo(() => ([{
-    id: '1',
-    label: 'Nhớ tài khoản',
-    value: 'nhotaikhoan',
-    color: '#37C5DF',
-    selectedColor: '#2CA9C0',
+        id: '1',
+        label: 'Nhớ tài khoản',
+        value: 'nhotaikhoan',
+        color: '#37C5DF',
+        selectedColor: '#2CA9C0',
     }]), []);
 
-const [selectedId, setSelectedId] = useState();
-
-return (
-    <View style={LoginStyle.container}>
-        <View style={LoginStyle.headerlogo}>
-            <Image style={LoginStyle.logo} source={require("../../../src/assets/logo.png")} />
-        </View>
-        <View style={LoginStyle.body}>
-            <View>
-                <View style={LoginStyle.tieude}>
-                    <Text style={LoginStyle.tieudedn}>Đăng nhập</Text>
-                    <Text style={LoginStyle.tieudednphu}>, để tiếp tục sử dụng</Text>
-                </View>
-                <View style={LoginStyle.inputall}>
-                    <Text style={LoginStyle.tieudeinput}>Địa chỉ email</Text>
-                    <View style={[LoginStyle.anhinput, emailError ? { borderColor: 'red', borderWidth: 1 } : {}]}>
-                        <Image style={LoginStyle.message} source={require("../../../src/assets/Message.png")} />
-                        <TextInput
-                            value={email}
-                            placeholder={emailError || "Nhập email"}
-                            placeholderTextColor={emailError ? 'red' : '#999'}
-                            onChangeText={(text) => {
-                                setEmail(text);
-                                setEmailError('');
-                                setLoginError('');
-                            }}
-                            style={[LoginStyle.input, emailError ? { color: 'red' } : {}]}
-                        />
+    return (
+        <View style={LoginStyle.container}>
+            <View style={LoginStyle.headerlogo}>
+                <Image style={LoginStyle.logo} source={require("../../../src/assets/logo.png")} />
+            </View>
+            <View style={LoginStyle.body}>
+                <View>
+                    <View style={LoginStyle.tieude}>
+                        <Text style={LoginStyle.tieudedn}>Đăng nhập</Text>
+                        <Text style={LoginStyle.tieudednphu}>, để tiếp tục sử dụng</Text>
                     </View>
-
-                    <Text style={LoginStyle.tieudeinput}>Mật khẩu</Text>
-                    <View style={[LoginStyle.anhinput, passwordError ? { borderColor: 'red', borderWidth: 1 } : {}]}>
-                        <Image style={LoginStyle.lockalt} source={require("../../../src/assets/Lock_alt.png")} />
-                        <TextInput
-                            value={password}
-                            placeholder={passwordError || "Nhập mật khẩu"}
-                            placeholderTextColor={passwordError ? 'red' : '#999'}
-                            onChangeText={(text) => {
-                                setPassword(text);
-                                setPasswordError('');
-                                setLoginError('');
-                            }}
-                            style={[LoginStyle.input, passwordError ? { color: 'red' } : {}]}
-                            secureTextEntry={!isPasswordVisible}
-                        />
-                        <TouchableOpacity onPress={() => setIsPasswordVisible(!isPasswordVisible)}>
-                            <Image
-                                style={LoginStyle.eye_closed}
-                                source={isPasswordVisible
-                                    ? require("../../../src/assets/eye.png")
-                                    : require("../../../src/assets/eye-closed.png")}
+                    <View style={LoginStyle.inputall}>
+                        <Text style={LoginStyle.tieudeinput}>Địa chỉ email</Text>
+                        <View style={[LoginStyle.anhinput, emailError ? { borderColor: 'red', borderWidth: 1 } : {}]}>
+                            <Image style={LoginStyle.message} source={require("../../../src/assets/Message.png")} />
+                            <TextInput
+                                value={email}
+                                placeholder={emailError || "Nhập email"}
+                                placeholderTextColor={emailError ? 'red' : '#999'}
+                                onChangeText={(text) => {
+                                    setEmail(text);
+                                    setEmailError('');
+                                    setLoginError('');
+                                }}
+                                style={[LoginStyle.input, emailError ? { color: 'red' } : {}]}
                             />
-                        </TouchableOpacity>
+                        </View>
+                        <Text style={LoginStyle.tieudeinput}>Mật khẩu</Text>
+                        <View style={[LoginStyle.anhinput, passwordError ? { borderColor: 'red', borderWidth: 1 } : {}]}>
+                            <Image style={LoginStyle.lockalt} source={require("../../../src/assets/Lock_alt.png")} />
+                            <TextInput
+                                value={password}
+                                placeholder={passwordError || "Nhập mật khẩu"}
+                                placeholderTextColor={passwordError ? 'red' : '#999'}
+                                onChangeText={handlePasswordChange} 
+                                style={[LoginStyle.input, passwordError ? { color: 'red' } : {}]}
+                                secureTextEntry={!isPasswordVisible}
+                            />
+                            <TouchableOpacity onPress={() => setIsPasswordVisible(!isPasswordVisible)}>
+                                <Image
+                                    style={LoginStyle.eye_closed}
+                                    source={isPasswordVisible
+                                        ? require("../../../src/assets/eye.png")
+                                        : require("../../../src/assets/eye-closed.png")}
+                                />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
-            </View>
-
-            <View style={LoginStyle.nhotk}>
-                <RadioGroup
-                    labelStyle={LoginStyle.radio}
-                    containerStyle={{ alignItems: 'flex-start' }}
-                    radioButtons={radioButtons}
-                    onPress={setSelectedId}
-                    selectedId={selectedId}
-                />
-                <TouchableOpacity onPress={ClickForgotPass}>
-                    <Text style={LoginStyle.forgot}>Quên mật khẩu?</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View style={LoginStyle.button}>
-                <TouchableOpacity onPress={BtnLogin} style={LoginStyle.dn}>
-                    <Text style={LoginStyle.chudn}>Đăng nhập</Text>
-                </TouchableOpacity>
-            </View>
-
-            <Text style={LoginStyle.or}>Hoặc</Text>
-
-            <View style={LoginStyle.buttonall}>
-                <TouchableOpacity style={LoginStyle.buttonfb}>
-                    <Image style={LoginStyle.fb} source={require('../../../src/assets/fb.png')} />
-                </TouchableOpacity>
-
-                <TouchableOpacity style={LoginStyle.buttongg}>
-                    <Image style={LoginStyle.gg} source={require("../../../src/assets/gg.png")} />
-                </TouchableOpacity>
-            </View>
-
-            <View style={LoginStyle.footer}>
-                <Text style={LoginStyle.footerdau}>Bạn chưa có tài khoản?</Text>
-                <Text onPress={ClickRegister} style={LoginStyle.footerduoi}> Đăng ký</Text>
+                <View style={LoginStyle.nhotk}>
+                    <RadioGroup
+                        labelStyle={LoginStyle.radio}
+                        containerStyle={{ alignItems: 'flex-start' }}
+                        radioButtons={radioButtons}
+                        onPress={setSelectedId} // Cần định nghĩa selectedId
+                        selectedId={selectedId} // Cần định nghĩa selectedId
+                    />
+                    <TouchableOpacity onPress={ClickForgotPass}>
+                        <Text style={LoginStyle.forgot}>Quên mật khẩu?</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={LoginStyle.button}>
+                    <TouchableOpacity onPress={BtnLogin} style={LoginStyle.dn}>
+                        <Text style={LoginStyle.chudn}>Đăng nhập</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text style={LoginStyle.or}>Hoặc</Text>
+                <View style={LoginStyle.buttonall}>
+                    <TouchableOpacity style={LoginStyle.buttonfb}>
+                        <Image style={LoginStyle.fb} source={require('../../../src/assets/fb.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={LoginStyle.buttongg}>
+                        <Image style={LoginStyle.gg} source={require("../../../src/assets/gg.png")} />
+                    </TouchableOpacity>
+                </View>
+                <View style={LoginStyle.footer}>
+                    <Text style={LoginStyle.footerdau}>Bạn chưa có tài khoản?</Text>
+                    <Text onPress={() => prop.navigation.navigate('Register')} style={LoginStyle.footerduoi}> Đăng ký</Text>
+                </View>
             </View>
         </View>
-    </View>
-);
+    );
 };
 
 export default Login;
--- src/Page/Register/Register.js
+++ src/Page/Register/Register.js
@@ -1,4 +1,4 @@
-import { Text, View, Image, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
+import { Text, View, Image, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform, Alert } from 'react-native'
 import React, { useMemo, useState } from 'react'
 import { RadioGroup } from 'react-native-radio-buttons-group'
 import RegisterStyle from './style'
@@ -14,13 +14,12 @@ const Register = (prop) => {
     const [emailError, setEmailError] = useState('');
     const [phoneError, setPhoneError] = useState('');
     const [passwordError, setPasswordError] = useState('');
+    const [selectedId, setSelectedId] = useState();
     const NextLogin = () => {
         prop.navigation.navigate('Login')
     }
 
     const BtnRegister = () => {
-        prop.navigation.navigate('SMS');
-
         let hasError = false;
 
         setFullNameError('');
@@ -28,48 +27,70 @@ const Register = (prop) => {
         setPhoneError('');
         setPasswordError('');
 
-        if (fullName === '') {
+        if (fullName.trim() === '') {
             setFullNameError("Vui lòng nhập họ và tên");
             setFullName('');
             hasError = true;
         }
 
-        if (email === '') {
+        if (email.trim() === '') {
             setEmailError("Vui lòng nhập email");
             setEmail('');
             hasError = true;
         } else if (!validateEmail(email)) {
-            setEmailError("Email không hợp lệ!");
+            setEmailError("Email không hợp lệ");
             setEmail('');
             hasError = true;
         }
 
-        if (phone === '') {
+        if (phone.trim() === '') {
             setPhoneError("Vui lòng nhập số điện thoại");
             setPhone('');
             hasError = true;
         } else if (!validatePhone(phone)) {
-            setPhoneError("Số điện thoại không hợp lệ!");
+            setPhoneError("Số điện thoại không hợp lệ");
             setPhone('');
             hasError = true;
         }
 
-        if (password === '') {
+        if (password.trim() === '') {
             setPasswordError("Vui lòng nhập mật khẩu");
             setPassword('');
             hasError = true;
-        } else if (password.length < 6) {
-            setPasswordError("Mật khẩu phải có ít nhất 6 ký tự");
-            setPassword('');
-            hasError = true;
+        } else {
+            let passwordErrors = [];
+
+            if (password.length < 8) {
+                passwordErrors.push("Mật khẩu phải có ít nhất 8 ký tự.");
+            }
+
+            if (!/[A-Z]/.test(password)) {
+                passwordErrors.push("Mật khẩu phải có ít nhất 1 chữ cái in hoa.");
+            }
+
+            if (!/\d/.test(password)) {
+                passwordErrors.push("Mật khẩu phải có ít nhất 1 số.");
+            }
+
+            if (!/[@$!%*?&]/.test(password)) {
+                passwordErrors.push("Mật khẩu phải có ít nhất 1 ký tự đặc biệt.");
+            }
+
+            if (passwordErrors.length > 0) {
+                setPasswordError(passwordErrors.join(" "));
+                setPassword('');
+                hasError = true;
+            }
         }
 
-        if (hasError) {
-            return;
+        if (!hasError) {
+            Alert.alert("Thông báo", "Đăng kí thành công!");
+            setTimeout(() => {
+                prop.navigation.navigate('SMS');
+            }, 1000);
         }
     };
 
-    // Kiểm tra email hợp lệ
     const validateEmail = (email) => {
         const re = /\S+@\S+\.\S+/;
         return re.test(email);
@@ -77,9 +98,15 @@ const Register = (prop) => {
 
     // Kiểm tra số điện thoại hợp lệ
     const validatePhone = (phone) => {
-        const re = /^[0-9]{10,11}$/; // Kiểm tra số điện thoại có 10-11 chữ số
+        const re = /^[0-9]{10,11}$/;
         return re.test(phone);
     };
+
+    const handlePasswordChange = (text) => {
+        setPassword(text);
+        setPasswordError('');
+    };
+
 
     const radioButtons = useMemo(() => ([{
         id: '1',
@@ -88,8 +115,6 @@ const Register = (prop) => {
         color: '#37C5DF',
         selectedColor: '#2CA9C0',
     }]), []);
-
-    const [selectedId, setSelectedId] = useState();
 
     return (
         <KeyboardAvoidingView
@@ -114,7 +139,7 @@ const Register = (prop) => {
                             <Image style={{ tintColor: "#2CA9C0", height: 24, width: 24 }} source={require("../../../src/assets/User_alt.png")} />
                             <TextInput
                                 value={fullName}
-                                placeholder={fullNameError ? "Họ và tên không hợp lệ!" : "Nhập họ và tên"}
+                                placeholder={fullNameError ? "Vui lòng nhập họ tên!" : "Nhập họ và tên"}
                                 placeholderTextColor={fullNameError ? 'red' : '#999'}
                                 onChangeText={(text) => {
                                     setFullName(text);
@@ -129,7 +154,7 @@ const Register = (prop) => {
                             <Image style={RegisterStyle.lockalt} source={require("../../../src/assets/Message.png")} />
                             <TextInput
                                 value={email}
-                                placeholder={emailError ? "Email không hợp lệ!" : "Nhập email"}
+                                placeholder={emailError || "Nhập email"}
                                 placeholderTextColor={emailError ? 'red' : '#999'}
                                 onChangeText={(text) => {
                                     setEmail(text);
@@ -142,15 +167,15 @@ const Register = (prop) => {
                         <Text style={RegisterStyle.tieudeInput}>Số điện thoại</Text>
                         <View style={[RegisterStyle.anhinput, phoneError ? { borderColor: 'red', borderWidth: 1 } : {}]}>
                             <Image style={{ tintColor: '#2CA9C0', width: 24, height: 24 }} source={require("../../../src/assets/Phone_fill.png")} />
-                            <TextInput 
-                            value={phone}
-                            placeholder={phoneError ? "Số điện thoại không hợp lệ!" : "Nhập số điện thoại"}
-                            placeholderTextColor={phoneError ? 'red' : '#999'}
-                            onChangeText={(text) => {
-                                setPhone(text);
-                                setPhoneError(''); 
-                            }}
-                            style={[RegisterStyle.input, phoneError ? { color: 'red' } : {}]}
+                            <TextInput
+                                value={phone}
+                                placeholder={phoneError || "Nhập số điện thoại"}
+                                placeholderTextColor={phoneError ? 'red' : '#999'}
+                                onChangeText={(text) => {
+                                    setPhone(text);
+                                    setPhoneError('');
+                                }}
+                                style={[RegisterStyle.input, phoneError ? { color: 'red' } : {}]}
                             />
                         </View>
 
@@ -158,15 +183,12 @@ const Register = (prop) => {
                         <View style={[RegisterStyle.anhinput, passwordError ? { borderColor: 'red', borderWidth: 1 } : {}]}>
                             <Image style={RegisterStyle.lockalt} source={require("../../../src/assets/Lock_alt.png")} />
                             <TextInput
-                               value={password}
-                               placeholder={passwordError ? "Mật khẩu không hợp lệ!" : "Nhập mật khẩu"}
-                               placeholderTextColor={passwordError ? 'red' : '#999'}
-                               onChangeText={(text) => {
-                                   setPassword(text);
-                                   setPasswordError('');
-                               }}
-                               style={[RegisterStyle.input, passwordError ? { color: 'red' } : {}]}
-                               secureTextEntry={!isPasswordVisible}
+                                value={password}
+                                placeholder={passwordError || "Nhập mật khẩu"}
+                                placeholderTextColor={passwordError ? 'red' : '#999'}
+                                onChangeText={handlePasswordChange} 
+                                style={[RegisterStyle.input, passwordError ? { color: 'red' } : {}]}
+                                secureTextEntry={!isPasswordVisible}
                             />
                             <TouchableOpacity onPress={() => setIsPasswordVisible(!isPasswordVisible)}>
                                 <Image
