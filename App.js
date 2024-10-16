80774a97dda65211ded4b7fbbfd019d45e055a51
LeDat1703 <130145289+LeDat1703@users.noreply.github.com>
2024-10-16 23:01:18 +0700

    policy3file

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainStack from './router/MainStack';
+import Order from './src/Page/Order/Order';
+import Policy from './src/Page/Policy/Policy';
+import { View } from 'react-native';
 const App = () => {
   return (
     <NavigationContainer>
-        <MainStack />
+        <MainStack/>
     </NavigationContainer>
+    // <View>
+    //   <Order/>
+    // </View>
+
   )  
 };
 
