000666978326293aef2c426ae72aa0677b877f8c
Kimtoann4 <130145513+Kimtoann4@users.noreply.github.com>
2024-10-17 21:15:54 +0700

    preserve

---
+++ src/Page/Preserve/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Preserve = () => {
+  return (
+    <View>
+      <Text>Preserve</Text>
+    </View>
+  )
+}
+
+export default Preserve
