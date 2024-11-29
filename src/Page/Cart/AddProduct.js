f9ab10c7d7190c1361c6e7beaee7f3a53204531b
ngocphat922004 <130296645+ngocphat922004@users.noreply.github.com>
2024-11-29 22:26:38 +0700

    .

--- src/Page/Cart/AddProduct.js
+++ src/Page/Cart/AddProduct.js
@@ -1,19 +1,12 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import {useSelector} from 'react-redux';
-import {
-  View,
-  Text,
-  FlatList,
-  Image,
-  TouchableOpacity,
-  Modal,
-  Alert,
-  ActivityIndicator,
-} from 'react-native';
+import {View, Text, FlatList, Image, TouchableOpacity, Modal, Alert, ActivityIndicator,} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import PayMethodStyle from '../Payment/PayMethod/style';
 import AddProductStyle from './AddProductStyle';
 import axiosInstance from '../api/AxiosInstance';
+import { useFocusEffect } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
 
 const CartItem = React.memo(({ item, toggleSelect, updateQuantity }) => {
     if (!item) return null;
@@ -42,13 +35,13 @@ const CartItem = React.memo(({ item, toggleSelect, updateQuantity }) => {
                 <Text style={AddProductStyle.itemCategory}>{item.category_name || 'Không có danh mục'}</Text>
                 <View>
                     <Text style={{ fontSize: 14 }}>
-                        {fixedPrice.toLocaleString()}đ
+                        {fixedPrice.toLocaleString()}.000đ
                     </Text>
                 </View>
                 <Text style={AddProductStyle.itemPrice}>
                     {(item.price && item.quantity) ?
-                        ((item.price ?? 0) * (item.quantity ?? 1)).toLocaleString() : 'Không có giá hoặc số lượng'}đ
-
+                        ((item.price ?? 0) * (item.quantity ?? 1)).toLocaleString() : 'Không có giá hoặc số lượng'}
+                    .000đ
                 </Text>
             </View>
             <View style={AddProductStyle.quantityContainer}>
@@ -60,30 +53,8 @@ const CartItem = React.memo(({ item, toggleSelect, updateQuantity }) => {
                     <Text style={AddProductStyle.quantityText}>+</Text>
                 </TouchableOpacity>
             </View>
-
         </View>
-        <Text style={AddProductStyle.itemPrice}>
-          {item.price && item.quantity
-            ? ((item.price ?? 0) * (item.quantity ?? 1)).toLocaleString()
-            : 'Không có giá hoặc số lượng'}
-          .000đ
-        </Text>
-      </View>
-      <View style={AddProductStyle.quantityContainer}>
-        <TouchableOpacity
-          onPress={() => updateQuantity(item.cart_id, 'decrease')}
-          style={AddProductStyle.quantityButton}>
-          <Text style={AddProductStyle.quantityText}>-</Text>
-        </TouchableOpacity>
-        <Text style={AddProductStyle.quantity}>{item.quantity}</Text>
-        <TouchableOpacity
-          onPress={() => updateQuantity(item.cart_id, 'increase')}
-          style={AddProductStyle.quantityButton}>
-          <Text style={AddProductStyle.quantityText}>+</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    );
 });
 
 const ConfirmationModal = ({visible, onConfirm, onCancel}) => (
@@ -268,7 +239,14 @@ const AddProduct = ({route, navigation}) => {
     const selectedItems = cartItems.filter(item => item.selected);
 
     if (selectedItems.length === 0) {
-      Alert.alert('Thông báo', 'Không có sản phẩm để xóa');
+        Toast.show({
+            type: 'error',
+            position: 'top',
+            text1: 'Thông báo',
+            text2: 'Không có sản phẩm để xóa',
+            visibilityTime: 3000,
+            autoHide: true,
+          });
     } else {
       try {
         for (const item of selectedItems) {
@@ -280,7 +258,93 @@ const AddProduct = ({route, navigation}) => {
             );
           }
         }
+        setModalVisible(false);
+        Toast.show({
+            type: 'success',
+            position: 'top',
+            text1: 'Thông báo',
+            text2: 'Xóa sản phẩm thành công',
+            visibilityTime: 3000,
+            autoHide: true,
+          });
 
+      } catch (error) {
+        Alert.alert('Lỗi', 'Có lỗi xảy ra khi xóa sản phẩm.');
+        console.log('Lỗi khi xóa sản phẩm', error);
+      }
+    }
+  };
+
+  const confirmDelete = () => {
+    const selectedItems = cartItems.filter(item => item.selected);
+    console.log('Items to confirm delete:', selectedItems);
+    if (selectedItems.length === 0) {
+      Alert.alert('Thông báo', 'Chưa chọn sản phẩm để xóa');
+    } else {
+      setModalVisible(true);
+    }
+  };
+
+  const handlePayment = async () => {
+    const selectedCartIds = cartItems
+      .filter(item => item.selected)
+      .map(item => item.cart_id);
+
+    console.log('Selected Cart IDs:', selectedCartIds);
+
+    if (selectedCartIds.length > 0) {
+      await AsyncStorage.setItem(
+        'selectedCartIds',
+        JSON.stringify(selectedCartIds),
+      );
+      console.log('Cart IDs saved to AsyncStorage');
+      const storedCartIds = await AsyncStorage.getItem('selectedCartIds');
+      console.log('Stored Cart IDs from AsyncStorage:', storedCartIds);
+      navigation.navigate('NextPayment', {cartIds: selectedCartIds});
+    } else {
+      Alert.alert('Thông báo', 'Chưa chọn sản phẩm để thanh toán');
+    }
+  };
+
+  return (
+    <View style={AddProductStyle.container}>
+      <View style={AddProductStyle.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image source={require('../../../src/assets/notifi/backright.png')} />
+        </TouchableOpacity>
+        <Text style={AddProductStyle.title}>Giỏ hàng</Text>
+        <TouchableOpacity
+          style={AddProductStyle.iconTrash}
+          onPress={confirmDelete}>
+          <Image source={require('../../../src/assets/Trash.png')} />
+        </TouchableOpacity>
+      </View>
+
+      {loading ? (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text>Đang tải giỏ hàng...</Text>
+        </View>
+      ) : cartItems.length === 0 ? (
+        <View
+          style={{
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 18, color: '#666'}}>
+            Hiện chưa có sản phẩm!!!
+          </Text>
+        </View>
+      ) : (
+        <FlatList
+          data={cartItems}
+          renderItem={({item}) => (
+            <CartItem
+              item={item}
+              toggleSelect={toggleSelectProduct}
+              updateQuantity={updateQuantity}
             />
           )}
           keyExtractor={(item, index) => `${item.cart_id}-${index}`}
--- src/Page/Order/Order.js
+++ src/Page/Order/Order.js
@@ -15,316 +15,317 @@ const Order = ({ navigation, route }) => {
 
   const tabs = ['Chờ xác nhận', 'Đang giao', 'Đã nhận', 'Đã hủy'];
 
-    const fetchOrders = useCallback(async () => {
-      setLoading(true);
-      try {
-        const response = await axiosInstance.get(`/oder/getorderbyuserid/${userid}`);
-        const allOrders = response;
+  const fetchOrders = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await axiosInstance.get(`/oder/getorderbyuserid/${userid}`);
+      const allOrders = response;
 
-        const filteredOrders = allOrders
-          .filter(order => {
-            switch (tabs[selectedTabs]) {
-              case 'Chờ xác nhận':
-                return order.status === 1;
-              case 'Đang giao':
-                return order.status === 2;
-              case 'Đã nhận':
-                return order.status === 3;
-              case 'Đã hủy':
-                return order.status === 4;
-              default:
-                return true;
-            }
-          })
-          .map(order => ({
-            ...order,
-            products: order.cart?.flatMap(cartItem => cartItem.products) || [],
-          }));
-
-        setOrders(filteredOrders);
-      } catch (error) {
-
-      } finally {
-        setLoading(false);
-      }
-    }, [selectedTabs, userid]);
-
-    useFocusEffect(
-      useCallback(() => {
-        fetchOrders();
-      }, [fetchOrders])
-    );
-
-    useEffect(() => {
-      const { selectedTab } = route.params || {};
-      if (selectedTab !== undefined) {
-        setSelectedTabs(selectedTab);
-      }
-    }, [route.params]);
-
-    const getStatusColor = (status) => {
-      switch (status) {
-        case 'Đang vận chuyển':
-          return '#4CAF50';
-        case 'Đã hủy':
-          return '#FF3434';
-        default:
-          return '#FF7400';
-      }
-    };
-
-    const addToCartHandler = async (order) => {
-      setFocusedOrder(order._id);
-      const productsToAdd = Array.isArray(order.products) ? order.products.map(product => ({
-        id: product._id,
-        name: product.name,
-        price: product.price,
-        quantity: product.quantity,
-        category: product.category,
-        images: product.images,
-        selected: true,
-      })) : [];
-      try {
-        const response = await axiosInstance.post('/carts/addCart_App', {
-          user: userid,
-          products: productsToAdd,
-        });
-
-        if (response.data.error) {
-          Toast.show({
-            type: 'error',
-            text1: 'Thông báo',
-            text2: 'Có lỗi xảy ra khi thêm sản phẩm!',
-            visibilityTime: 2000,
-            position: 'top',
-          });
-        } else {
-          Toast.show({
-            type: 'success',
-            text1: 'Thông báo',
-            text2: 'Thêm sản phẩm thành công!',
-            visibilityTime: 2000,
-            position: 'top',
-          });
-          navigation.navigate('AddProduct');
-        }
-      } catch (error) {
-        console.error('Failed to add to cart:', error);
-      }
-    };
-
-    const renderOrderCard = ({ item }) => (
-      <TouchableOpacity
-        onPress={() => {
-          const screenMapping = {
-            'Chờ xác nhận': 'Processing1',
-            'Đang giao': 'Delivering',
-            'Đã nhận': 'Done',
-            'Đã hủy': 'Canceled',
-          };
-          const screenName = screenMapping[tabs[selectedTabs]];
-          if (screenName) {
-            navigation.navigate(screenName, { order: item });
+      const filteredOrders = allOrders
+        .filter(order => {
+          switch (tabs[selectedTabs]) {
+            case 'Chờ xác nhận':
+              return order.status === 1;
+            case 'Đang giao':
+              return order.status === 2;
+            case 'Đã nhận':
+              return order.status === 3;
+            case 'Đã hủy':
+              return order.status === 4;
+            default:
+              return true;
           }
-        }}
-      >
-        <View style={OrderStyle.orderCard}>
-          <View style={OrderStyle.imageContainer}>
-            <View style={OrderStyle.borderimage}>
-              {item.products.length > 0 && item.products[0].images?.length > 0 ? (
-                <Image source={{ uri: item.products[0].images[0] }} style={OrderStyle.image} />
-              ) : (
-                <View style={[OrderStyle.image, { backgroundColor: '#cccccc' }]} />
-              )}
-            </View>
-            {item.status === 'Đã nhận' && <Text style={OrderStyle.completedText}>Hoàn thành</Text>}
-          </View>
-          <View style={OrderStyle.orderInfo}>
-            <Text style={OrderStyle.orderName}>{item.products.length > 0 ? item.products[0].name : 'Không có sản phẩm'}</Text>
-            <Text style={OrderStyle.orderQuantity}>SL: {item.products.length > 0 ? item.products[0].quantity : 0}</Text>
-            <Text style={OrderStyle.orderPrice}>Tổng tiền: {Math.round(item.totalOrder).toLocaleString('vi-VN')}.000 đ</Text>
+        })
+        .map(order => ({
+          ...order,
+          products: order.cart?.flatMap(cartItem => cartItem.products) || [],
+        }));
 
-            {tabs[selectedTabs] !== 'Đã nhận' && (
-              <Text style={[OrderStyle.orderStatus, { color: getStatusColor(item.status) }]}>{item.status}</Text>
-            )}
+      setOrders(filteredOrders);
+    } catch (error) {
 
-            {tabs[selectedTabs] === 'Đã nhận' && (
-              <View style={OrderStyle.buttonContainer}>
-                <TouchableOpacity onPress={() => addToCartHandler(item)} style={OrderStyle.buttonnhan}>
-                  <Text style={OrderStyle.buttonTextnhan}>Mua lại</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('ProductReview', { order: item })} style={OrderStyle.buttonhuy}>
-                  <Text style={OrderStyle.buttonTexthuy}>Đánh giá</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-          </View>
-        </View>
+    } finally {
+      setLoading(false);
+    }
+  }, [selectedTabs, userid]);
 
-    return (
-      <View style={OrderStyle.container}>
-        <View style={OrderStyle.header}>
-          <TouchableOpacity onPress={() => navigation.goBack()}
-            style={{
-              position: 'absolute',
-              left: 0,
-              top: 0,
-            }} >
-            <Image source={require('../../assets/notifi/backright.png')} />
-          </TouchableOpacity>
-          <Text style={OrderStyle.title}>Đơn hàng</Text>
-        </View>
+  useFocusEffect(
+    useCallback(() => {
+      fetchOrders();
+    }, [fetchOrders])
+  );
 
-        <View style={OrderStyle.tabsContainer}>
-          {tabs.map((tab, index) => (
-            <TouchableOpacity
-              key={index}
-              style={[OrderStyle.tabsButton, selectedTabs === index && OrderStyle.selectedTabsButton]}
-              onPress={() => setSelectedTabs(index)}
-            >
-              <Text style={{
-                fontSize: 16,
-                fontWeight: selectedTabs === index ? 'bold' : 'normal',
-                textDecorationLine: selectedTabs === index ? 'underline' : 'none',
-                color: 'black',
-                textAlign: 'center',
-              }}>
-                {tab}
-              </Text>
-            </TouchableOpacity>
-          ))}
-        </View>
+  useEffect(() => {
+    const { selectedTab } = route.params || {};
+    if (selectedTab !== undefined) {
+      setSelectedTabs(selectedTab);
+    }
+  }, [route.params]);
 
-        {loading ? (
-          <ActivityIndicator size="large" color="#0000ff" />
-        ) : (
-          <FlatList
-            data={orders}
-            renderItem={renderOrderCard}
-            keyExtractor={item => item._id?.toString() || Math.random().toString()}
-            contentContainerStyle={{ paddingBottom: 100 }}
-            showsVerticalScrollIndicator={false}
-          />
-        )}
-      </View>
-    );
+  const getStatusColor = (status) => {
+    switch (status) {
+      case 'Đang vận chuyển':
+        return '#4CAF50';
+      case 'Đã hủy':
+        return '#FF3434';
+      default:
+        return '#FF7400';
+    }
   };
 
-  const OrderStyle = StyleSheet.create({
-    container: {
-      padding: 20,
-      flex: 1,
-    },
-    header: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center'
+  const addToCartHandler = async (order) => {
+    setFocusedOrder(order._id);
+    const productsToAdd = Array.isArray(order.products) ? order.products.map(product => ({
+      id: product._id,
+      name: product.name,
+      price: product.price,
+      quantity: product.quantity,
+      category: product.category,
+      images: product.images,
+      selected: true,
+    })) : [];
+    try {
+      const response = await axiosInstance.post('/carts/addCart_App', {
+        user: userid,
+        products: productsToAdd,
+      });
 
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
+      if (response.data.error) {
+        Toast.show({
+          type: 'error',
+          text1: 'Thông báo',
+          text2: 'Có lỗi xảy ra khi thêm sản phẩm!',
+          visibilityTime: 2000,
+          position: 'top',
+        });
+      } else {
+        Toast.show({
+          type: 'success',
+          text1: 'Thông báo',
+          text2: 'Thêm sản phẩm thành công!',
+          visibilityTime: 2000,
+          position: 'top',
+        });
+        navigation.navigate('AddProduct');
+      }
+    } catch (error) {
+      console.error('Failed to add to cart:', error);
+    }
+  };
 
-      color: 'black',
-      marginLeft: 10,
-    },
-    backButton: {
-      padding: 8,
-    },
-    backIcon: {
-      width: 24,
-      height: 24,
-      tintColor: 'black',
-    },
-    orderCard: {
-      marginTop: 14,
-      flexDirection: 'row',
-      padding: 8,
-      borderColor: '#BBAFAF',
-      borderWidth: 1,
-      borderRadius: 8,
-      alignItems: 'center',
-    },
-    borderimage: {
-      borderRadius: 10,
-      backgroundColor: '#73D6E9',
-      width: 80,
-      height: 80,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    image: {
-      width: 60,
-      height: 60,
-      borderRadius: 8,
-    },
-    completedText: {
-      fontSize: 14,
-      color: '#1976D2',
-      marginTop: 10,
-    },
-    orderInfo: {
-      marginLeft: 16,
-      flex: 1,
-    },
-    orderName: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      color: 'black',
-    },
-    orderQuantity: {
-      fontSize: 14,
-      color: 'black',
-    },
-    orderPrice: {
-      fontSize: 14,
-      color: '#FF3434',
-    },
-    orderStatus: {
-      fontSize: 14,
-      alignSelf: 'flex-end',
-    },
-    tabsContainer: {
-      flexDirection: 'row',
-      marginVertical: 10,
-      justifyContent: "space-between"
-    },
-    buttonContainer: {
-      flexDirection: 'row',
-      marginTop: 20,
-      justifyContent: 'flex-end',
-    },
-    buttonnhan: {
-      backgroundColor: 'white',
-      padding: 5,
-      borderRadius: 5,
-      marginRight: 5,
-      borderWidth: 1,
-      width: 80,
-      height: 34,
-      borderColor: '#BBAFAF',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    buttonhuy: {
-      backgroundColor: 'white',
-      padding: 5,
-      borderWidth: 1,
-      borderRadius: 5,
-      width: 80,
-      height: 34,
-      borderColor: '#FF7400',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    buttonTextnhan: {
-      color: 'black',
-      fontSize: 16,
-    },
-    buttonTexthuy: {
-      color: '#FF7400',
-      fontSize: 16,
-    },
-  });
+  const renderOrderCard = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => {
+        const screenMapping = {
+          'Chờ xác nhận': 'Processing1',
+          'Đang giao': 'Delivering',
+          'Đã nhận': 'Done',
+          'Đã hủy': 'Canceled',
+        };
+        const screenName = screenMapping[tabs[selectedTabs]];
+        if (screenName) {
+          navigation.navigate(screenName, { order: item });
+        }
+      }}
+    >
+      <View style={OrderStyle.orderCard}>
+        <View style={OrderStyle.imageContainer}>
+          <View style={OrderStyle.borderimage}>
+            {item.products.length > 0 && item.products[0].images?.length > 0 ? (
+              <Image source={{ uri: item.products[0].images[0] }} style={OrderStyle.image} />
+            ) : (
+              <View style={[OrderStyle.image, { backgroundColor: '#cccccc' }]} />
+            )}
+          </View>
+          {item.status === 'Đã nhận' && <Text style={OrderStyle.completedText}>Hoàn thành</Text>}
+        </View>
+        <View style={OrderStyle.orderInfo}>
+          <Text style={OrderStyle.orderName}>{item.products.length > 0 ? item.products[0].name : 'Không có sản phẩm'}</Text>
+          <Text style={OrderStyle.orderQuantity}>SL: {item.products.length > 0 ? item.products[0].quantity : 0}</Text>
+          <Text style={OrderStyle.orderPrice}>Tổng tiền: {Math.round(item.totalOrder).toLocaleString('vi-VN')}.000 đ</Text>
 
-  export default Order;
+          {tabs[selectedTabs] !== 'Đã nhận' && (
+            <Text style={[OrderStyle.orderStatus, { color: getStatusColor(item.status) }]}>{item.status}</Text>
+          )}
+
+          {tabs[selectedTabs] === 'Đã nhận' && (
+            <View style={OrderStyle.buttonContainer}>
+              <TouchableOpacity onPress={() => addToCartHandler(item)} style={OrderStyle.buttonnhan}>
+                <Text style={OrderStyle.buttonTextnhan}>Mua lại</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.navigate('ProductReview', { order: item })} style={OrderStyle.buttonhuy}>
+                <Text style={OrderStyle.buttonTexthuy}>Đánh giá</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+  return (
+    <View style={OrderStyle.container}>
+      <View style={OrderStyle.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}
+          style={{
+            position: 'absolute',
+            left: 0,
+            top: 0,
+          }} >
+          <Image source={require('../../assets/notifi/backright.png')} />
+        </TouchableOpacity>
+        <Text style={OrderStyle.title}>Đơn hàng</Text>
+      </View>
+
+      <View style={OrderStyle.tabsContainer}>
+        {tabs.map((tab, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[OrderStyle.tabsButton, selectedTabs === index && OrderStyle.selectedTabsButton]}
+            onPress={() => setSelectedTabs(index)}
+          >
+            <Text style={{
+              fontSize: 16,
+              fontWeight: selectedTabs === index ? 'bold' : 'normal',
+              textDecorationLine: selectedTabs === index ? 'underline' : 'none',
+              color: 'black',
+              textAlign: 'center',
+            }}>
+              {tab}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={orders}
+          renderItem={renderOrderCard}
+          keyExtractor={item => item._id?.toString() || Math.random().toString()}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          showsVerticalScrollIndicator={false}
+        />
+      )}
+    </View>
+  );
+};
+
+const OrderStyle = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+
+    color: 'black',
+    marginLeft: 10,
+  },
+  backButton: {
+    padding: 8,
+  },
+  backIcon: {
+    width: 24,
+    height: 24,
+    tintColor: 'black',
+  },
+  orderCard: {
+    marginTop: 14,
+    flexDirection: 'row',
+    padding: 8,
+    borderColor: '#BBAFAF',
+    borderWidth: 1,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  borderimage: {
+    borderRadius: 10,
+    backgroundColor: '#73D6E9',
+    width: 80,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+  },
+  completedText: {
+    fontSize: 14,
+    color: '#1976D2',
+    marginTop: 10,
+  },
+  orderInfo: {
+    marginLeft: 16,
+    flex: 1,
+  },
+  orderName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  orderQuantity: {
+    fontSize: 14,
+    color: 'black',
+  },
+  orderPrice: {
+    fontSize: 14,
+    color: '#FF3434',
+  },
+  orderStatus: {
+    fontSize: 14,
+    alignSelf: 'flex-end',
+  },
+  tabsContainer: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    justifyContent: "space-between"
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    justifyContent: 'flex-end',
+  },
+  buttonnhan: {
+    backgroundColor: 'white',
+    padding: 5,
+    borderRadius: 5,
+    marginRight: 5,
+    borderWidth: 1,
+    width: 80,
+    height: 34,
+    borderColor: '#BBAFAF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonhuy: {
+    backgroundColor: 'white',
+    padding: 5,
+    borderWidth: 1,
+    borderRadius: 5,
+    width: 80,
+    height: 34,
+    borderColor: '#FF7400',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTextnhan: {
+    color: 'black',
+    fontSize: 16,
+  },
+  buttonTexthuy: {
+    color: '#FF7400',
+    fontSize: 16,
+  },
+});
+
+export default Order;
