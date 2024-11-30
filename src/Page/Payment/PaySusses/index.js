6c07b3fdd821390cc2ef15c44226a869d3873f17
ngocphat922004 <130296645+ngocphat922004@users.noreply.github.com>
2024-11-30 15:51:44 +0700

    .

--- src/Page/Payment/PaySusses/index.js
+++ src/Page/Payment/PaySusses/index.js
@@ -1,24 +1,28 @@
-    import React from "react";
+import React, {useEffect} from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import PaySussesStyle from "./style"
 import PayMethodStyle from "../PayMethod/style"
 const PaySusses = (prop) => {
     const { navigation } = prop
     const HandNav = () => {
-        navigation.navigate('BottomNav')
-    }
+        setTimeout(() => {
+            navigation.navigate('BottomNav');
+        }, 2000); 
+    };
+    
+    useEffect(() => {
+        HandNav();
+    }, []);
     return (
         <View style={PaySussesStyle.container}>
+            
             <View style={PaySussesStyle.body}>
                 <Image source={require('../../../assets/notifi/PaySusses.png')} />
                 <TouchableOpacity>
-                    <Text style={PaySussesStyle.text}>Thanh toán thanh công</Text>
+                    <Text style={PaySussesStyle.text}>Thanh toán thành công</Text>
                 </TouchableOpacity>
             </View>
             <View style={[PayMethodStyle.ViewSuss, { padding: 20 }]}>
-                <TouchableOpacity onPress={HandNav} style={PayMethodStyle.BtnSuss}>
-                    <Text style={PayMethodStyle.txtSuss}>QUAY LẠI TRANG CHỦ</Text>
-                </TouchableOpacity>
             </View>
         </View>
     )
